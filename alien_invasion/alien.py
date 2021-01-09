93260c3a44c9784c9965407e87c8f86b9766a3f9
mroloff2019 <49107196+mroloff2019@users.noreply.github.com>
2021-01-08 20:51:18 -0800

    Added files via upload

---
+++ alien_invasion/alien.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.sprite import Sprite  
+
+class Alien(Sprite):
+	"""A class to represent a single alien the fleet."""
+
+	def __init__(self, ai_settings, screen):
+		"""initialize the alien and set its starting position."""
+		super(Alien, self).__init__()
+		self.screen = screen
+		self.ai_settings = ai_settings
+
+
+		#load.image and its rect attribute.
+		self.image = pygame.image.load('images/alien.bmp')
+		self.rect = self.image.get_rect()
+
+
+		#Start each new alien near the top let of the screen.
+		self.rect.x = self.rect.width
+		self.rect.y = self.rect.height
+
+
+		#Store the aliens exat position.
+		self.x = float(self.rect.x)
+
+	def blitme(self):
+		"""Draw the alien at its current location."""
+		self.screen.blit(self.image, self.rect)
+
+	def check_edges(self):
+		"""Return true if alien is at edge of the screen."""
+		screen_rect = self.screen.get_rect()
+		if self.rect.right >= screen_rect.right:
+			return True 
+		elif self.rect.left <= 0:
+			return True 
+
+	def update(self):
+		"""move the alien right or left"""
+		self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
+		self.rect.x = self.x
